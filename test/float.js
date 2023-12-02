8dd58991a52bd09fcd6d09a83b204e1ed918bdb9
Anders Rundgren <anders.rundgren.net@gmail.com>
2023-12-02 12:06:01 +0100

    updated float.js

--- test/float.js
+++ test/float.js
@@ -2,8 +2,9 @@
 import CBOR from '../npm/mjs/index.mjs';
 import { assertTrue, assertFalse, success } from './assertions.js';
 
-function oneTurn(value, expected) {
-  let text = value.toString();
+function oneTurn(valueText, expected) {
+  let value = Number(valueText);
+  let text = valueText;
   while (text.length < 25) {
     text += ' ';
   }
@@ -14,8 +15,14 @@ function oneTurn(value, expected) {
   } else {
     got = '';
   }
-  if (CBOR.decode(cbor).getFloat() != value) {
+  let decodedValue = CBOR.decode(cbor);
+  let nanFlag = expected == 'f97e00';
+  if ((nanFlag && decodedValue.toString() != "NaN") || 
+     (!nanFlag && decodedValue.getFloat() != value)) {
     throw Error("Failed decoding: " + value);
+  }
+  if (decodedValue.toString() != valueText) {
+    throw Error("Failed encoding: " + valueText + " " + decodedValue.toString());
   }
   while (expected.length < 20) {
     expected += ' ';
@@ -24,34 +31,38 @@ function oneTurn(value, expected) {
     throw Error(text + expected + got);
   }
 }
-oneTurn(6.10649585723877e-5, 'fa38801000');
-oneTurn(10.559998512268066, 'fa4128f5c1');
-oneTurn(65472.0, 'f97bfe');
-oneTurn(65472.00390625, 'fa477fc001');
-oneTurn(65503.0, 'fa477fdf00');
-oneTurn(65504.0, 'f97bff');
-oneTurn(65504.00390625, 'fa477fe001');
-oneTurn(65504.5, 'fa477fe080');
-oneTurn(65505.0, 'fa477fe100');
-oneTurn(131008.0, 'fa47ffe000');
-oneTurn(-5.960464477539062e-8, 'fbbe6fffffffffffff');
-oneTurn(-5.960464477539063e-8, 'f98001');
-oneTurn(-5.960464477539064e-8, 'fbbe70000000000001');
-oneTurn(-5.960465188081798e-8, 'fab3800001');
-oneTurn(-5.963374860584736e-8, 'fab3801000');
-oneTurn(-5.966285243630409e-8, 'fab3802000');
-oneTurn(-8.940696716308594e-8, 'fab3c00000');
-oneTurn(-0.00006097555160522461, 'f983ff');
-oneTurn(-0.00006097555160522469, 'fbbf0ff8000000000c');
-oneTurn(-0.000060975551605224615, 'fbbf0ff80000000001');
-oneTurn(-0.0000609755516052246127, 'f983ff');
-oneTurn(-0.0000609755516052246128, 'fbbf0ff80000000001');
-oneTurn(0.00006103515625, 'f90400');
-oneTurn(0.00006103515625005551, 'fb3f10000000001000');
-oneTurn(1.401298464324817e-45, 'fa00000001');
-oneTurn(1.1754942106924411e-38, 'fa007fffff');
-oneTurn(0.00006109476089477539, 'f90401');
-oneTurn(7.52316384526264e-37, 'fa03800000');
-oneTurn(1.1754943508222875e-38, 'fa00800000');
-
+oneTurn('0.0',                      'f90000');
+oneTurn('-0.0',                     'f98000');
+oneTurn('NaN',                      'f97e00');
+oneTurn('Infinity',                 'f97c00');
+oneTurn('-Infinity',                'f9fc00');
+oneTurn('0.0000610649585723877',    'fa38801000');
+oneTurn('10.559998512268066',       'fa4128f5c1');
+oneTurn('65472.0',                  'f97bfe');
+oneTurn('65472.00390625',           'fa477fc001');
+oneTurn('65503.0',                  'fa477fdf00');
+oneTurn('65504.0',                  'f97bff');
+oneTurn('65504.00390625',           'fa477fe001');
+oneTurn('65504.5',                  'fa477fe080');
+oneTurn('65505.0',                  'fa477fe100');
+oneTurn('131008.0',                 'fa47ffe000');
+oneTurn('-5.960464477539062e-8',    'fbbe6fffffffffffff');
+oneTurn('-5.960464477539063e-8',    'f98001');
+oneTurn('-5.960464477539064e-8',    'fbbe70000000000001');
+oneTurn('-5.960465188081798e-8',    'fab3800001');
+oneTurn('-5.963374860584736e-8',    'fab3801000');
+oneTurn('-5.966285243630409e-8',    'fab3802000');
+oneTurn('-8.940696716308594e-8',    'fab3c00000');
+oneTurn('-0.00006097555160522461',  'f983ff');
+oneTurn('-0.000060975551605224616', 'fbbf0ff80000000001');
+oneTurn('-0.000060975555243203416', 'fab87fc001');
+oneTurn('0.00006103515625',         'f90400');
+oneTurn('0.00006103515625005551',   'fb3f10000000001000');
+oneTurn('1.401298464324817e-45',    'fa00000001');
+oneTurn('1.1754942106924411e-38',   'fa007fffff');
+oneTurn('0.00006109476089477539',   'f90401');
+oneTurn('7.52316384526264e-37',     'fa03800000');
+oneTurn('1.1754943508222875e-38',   'fa00800000');
+oneTurn('5.0e-324',                 'fb0000000000000001');
+oneTurn('-1.7976931348623157e+308', 'fbffefffffffffffff');
 success();
