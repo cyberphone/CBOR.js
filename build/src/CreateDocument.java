19a9ddf906e142d060d118125011228185f6005d
Anders Rundgren <anders.rundgren.net@gmail.com>
2025-06-15 06:37:28 +0200

    CBOR::Core 11

--- build/src/CreateDocument.java
+++ build/src/CreateDocument.java
@@ -1699,7 +1699,7 @@ public class CreateDocument {
 
     replace(TOC, printTableOfContents());
 
-    replace(CBOR_CORE, "[<a href='https://www.ietf.org/archive/id/draft-rundgren-cbor-core-10.html' " +
+    replace(CBOR_CORE, "[<a href='https://www.ietf.org/archive/id/draft-rundgren-cbor-core-11.html' " +
                        "title='CBOR::Core'>CBOR::Core<img src='xtl.svg' alt='link'></a>]");
 
     IO.writeFile(documentFileName, template);
