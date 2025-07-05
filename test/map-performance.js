d2f5f7c2d63e44b8de2c640279410f809227ae57
Anders Rundgren <anders.rundgren.net@gmail.com>
2025-07-05 08:45:27 +0200

    trimming...

--- test/map-performance.js
+++ test/map-performance.js
@@ -9,13 +9,11 @@ const SMALL_MAP  = 10;
 const MEDIUM_MAP = 50;
     
 let SORTED_KEYS = [TOTAL_SET_OPERATIONS];
-let REVERSE_KEYS = [TOTAL_SET_OPERATIONS];
 let VALUES = [TOTAL_SET_OPERATIONS];
     
 for (let q = 0; q < TOTAL_SET_OPERATIONS; q++) {
-  let key = CBOR.String("prefix" + q);
+  let key = CBOR.Int(q);
   SORTED_KEYS[q] = key;
-  REVERSE_KEYS[TOTAL_SET_OPERATIONS - 1 - q] = key;
   VALUES[q] = CBOR.Int(q); 
 }
     
@@ -26,82 +24,93 @@ function printTime(label, mapSize, startTime, sortFlag) {
     
 function bigMap(sortFlag) {
   let startTime = new Date();
-  let cborMap = CBOR.Map().setSortingMode(sortFlag);
+  let map = CBOR.Map().setSortingMode(sortFlag);
         
   for (let q = 0; q < TOTAL_SET_OPERATIONS; q++) {
-    cborMap.set(SORTED_KEYS[q], VALUES[q]);
+    map.set(SORTED_KEYS[q], VALUES[q]);
   }
   printTime("SET", TOTAL_SET_OPERATIONS, startTime, sortFlag);
+
   startTime = new Date();
   for (let n = 0; n < TOTAL_SET_OPERATIONS ; n++) {
-    if (cborMap.get(SORTED_KEYS[n]).getInt53() != n) {
+    if (map.get(SORTED_KEYS[n]).getInt53() != n) {
       throw Error("Big access");
     }
   }
   printTime("GET", TOTAL_SET_OPERATIONS, startTime, sortFlag);
-  startTime = new Date();
+
   if (sortFlag) return;
-  cborMap = CBOR.Map();
-  for (let q = 0; q < TOTAL_SET_OPERATIONS; q++) {
-    cborMap.set(REVERSE_KEYS[q], VALUES[q]);
+
+  startTime = new Date();
+  map = CBOR.Map();
+  for (let n = TOTAL_SET_OPERATIONS; --n >= 0;) {
+    map.set(SORTED_KEYS[n], VALUES[n]);
   }
   printTime("Reverse SET", TOTAL_SET_OPERATIONS, startTime, sortFlag);
-  startTime = new Date();
-  for (let n = 0; n < TOTAL_SET_OPERATIONS ; n++) {
-    if (cborMap.get(REVERSE_KEYS[n]).getInt53() != n) {
-      throw Error("Big access");
-    }
-  }
-  printTime("Reverse GET", TOTAL_SET_OPERATIONS, startTime, sortFlag);
 }
     
 function multipleSmallMaps(mapSize, sortFlag) {
-  let cborMap = null;
+  let map = null;
   let startTime = new Date();
   let maps = TOTAL_SET_OPERATIONS / mapSize;
+
   for (let q = 0; q < maps; q++) {
     // Creating a CBORMap object is a heavy operation
-    cborMap = CBOR.Map().setSortingMode(sortFlag);
+    map = CBOR.Map().setSortingMode(sortFlag);
     for (let n = 0; n < mapSize; n++) {
-      cborMap.set(SORTED_KEYS[n], VALUES[n]);
+      map.set(SORTED_KEYS[n], VALUES[n]);
     }            
   }
   printTime("SET", mapSize, startTime, sortFlag);
+
   startTime = new Date();
   for (let q = 0; q < maps; q++) {
     for (let n = 0; n < mapSize; n++) {
-      if (cborMap.get(SORTED_KEYS[n]).getInt53() != n) {
+      if (map.get(SORTED_KEYS[n]).getInt53() != n) {
           throw Error("Medium access");
       }
     }            
   }
   printTime("GET", mapSize, startTime, sortFlag);
+
+  startTime = new Date();
+  for (let q = 0; q < maps; q++) {
+    map = [];
+    for (let n = 0; n < mapSize; n++) {
+      let key = SORTED_KEYS[n];
+      let entry = {"key": key,
+                   "value": VALUES[n],
+                   "encoded": key.encode()};
+      if (n) {
+        if (CBOR.compareArrays(map[n - 1].encoded, entry.encoded) > 0) {
+          throw Error("ordering or duplicate");
+        }
+      }
+      map[n] = entry;
+    }            
+  }
+  printTime("SET naked-map", mapSize, startTime, sortFlag);
+
   if (sortFlag) return;
+
   startTime = new Date();
   for (let q = 0; q < maps; q++) {
     // Creating a CBORMap object is a heavy operation
-    cborMap = CBOR.Map();
-    for (let n = 0; n < mapSize; n++) {
-      cborMap.set(REVERSE_KEYS[n], VALUES[n]);
+    map = CBOR.Map();
+    for (let n = mapSize; --n >= 0;) {
+      map.set(SORTED_KEYS[n], VALUES[n]);
     }            
   }
   printTime("Reverse SET", mapSize, startTime, sortFlag);
-  startTime = new Date();
-  for (let q = 0; q < maps; q++) {
-    for (let n = 0; n < mapSize; n++) {
-      if (cborMap.get(REVERSE_KEYS[n]).getInt53() != n) {
-          throw Error("Medium access");
-      }
-    }            
-  }
-  printTime("Reverse GET", mapSize, startTime, sortFlag);
 }
-    
-multipleSmallMaps(SMALL_MAP, false);
+
 multipleSmallMaps(SMALL_MAP, true);
-multipleSmallMaps(MEDIUM_MAP, false);
+multipleSmallMaps(SMALL_MAP, false);
+
 multipleSmallMaps(MEDIUM_MAP, true);
-bigMap(false);
+multipleSmallMaps(MEDIUM_MAP, false);
+
 bigMap(true);
+bigMap(false);
 
 success();
